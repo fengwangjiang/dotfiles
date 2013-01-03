8aafd93c49774515817399c3d98fd08fa2d6385f
Jason Knight <Jason@JasonKnight.us>
2013-01-03 12:44:02 -0600

    Added install list and scripts

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -ex
+
+# inspired from https://gist.github.com/2913223
+#
+# sudo apt-get update && sudo apt-get upgrade
+# cat install-list | sudo dpkg --set-selections
+# sudo apt-get install
+#
+# chsh --shell /bin/zsh
+#
+# curl -s https://raw.github.com/andsens/homeshick/master/install.sh | bash
+# $HOME/.homeshick clone git@github.com:binarybana/dotfiles.git
+# $HOME/.homeshick symlink dotfiles
+#
+# Now I need to get the rest of the files from:
+# bin .s3ql .unison .ssh GSP Dropbox
